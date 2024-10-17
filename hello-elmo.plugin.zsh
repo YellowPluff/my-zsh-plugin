5c51345ed633d3860897a6f13c4d9709ae0db014
Dakota Nickel <dakotanickel@Dakotas-MacBook-Pro.local>
2024-10-16 20:43:27 -0400

    Update Elmo to Barney

--- hello-elmo.plugin.zsh
+++ hello-elmo.plugin.zsh
@@ -1,4 +1,8 @@
+echo "This should emulate a plugin taking 20 seconds to load in..."
+sleep 20
+echo "Done sleeping.. Now will load..."
+
 function say_hello
 {
-    echo "Hello, Elmo!"
+    echo "Hello, I'm done napping!"
 }
