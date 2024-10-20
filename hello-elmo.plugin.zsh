b5a434107df2e254bc748aef0305fd33f8aca9eb
Dakota Nickel <dakotanickel@Dakotas-MacBook-Pro.local>
2024-10-20 17:08:15 -0400

    Removed the sleeping code

--- hello-elmo.plugin.zsh
+++ hello-elmo.plugin.zsh
@@ -1,8 +1,4 @@
-echo "This should emulate a plugin taking 20 seconds to load in..."
-sleep 20
-echo "Done sleeping.. Now will continue loading..."
-
 function say_hello
 {
-    echo "Hello, I'm done napping!"
+    echo "Hello, I don't like to nap anymore..."
 }
