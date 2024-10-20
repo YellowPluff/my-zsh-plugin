03f40de88264949ec892a4fbf45b22b9272ce297
Dakota Nickel <dakotanickel@Dakotas-MacBook-Pro.local>
2024-10-20 17:38:17 -0400

    Testing zinit update time

--- hello-elmo.plugin.zsh
+++ hello-elmo.plugin.zsh
@@ -1,6 +1,4 @@
 function say_hello
 {
-    echo "Hello, I don't like to nap anymore... but now I was updated..."
-    echo "I removed the --all -f from the .zshrc"
-    echo "Hmm>>/"
+    echo "I got updated..."
 }
