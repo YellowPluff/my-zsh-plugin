5d61a79430f38b764c71c89266f1d9a11ac5b13a
Dakota Nickel <dakotanickel@Dakotas-MacBook-Pro.local>
2024-10-20 17:00:06 -0400

    New Commit 1 ...

--- hello-elmo.plugin.zsh
+++ hello-elmo.plugin.zsh
@@ -1,6 +1,6 @@
 echo "This should emulate a plugin taking 20 seconds to load in..."
 sleep 20
-echo "Done sleeping.. Now will load..."
+echo "Done sleeping.. Now will continue loading..."
 
 function say_hello
 {
